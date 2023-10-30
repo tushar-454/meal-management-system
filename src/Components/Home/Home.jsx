9a220659569257515536aef510f04f93942adb50
tushar-454 <imtushar454@gmail.com>
2023-10-30 07:52:18 +0600

    if email not verified show modal. this modal ui design completed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,28 @@
+import resend from '../../assets/icon/resend.png';
 import Container from '../Reusable/Container';
+import LinkButton from '../UI/LinkButton';
+import styles from './Home.module.css';
 
 const Home = () => {
+  const isEmailVarified = false;
   return (
     <section>
       <Container>
         <h1>This is my home Components</h1>
       </Container>
+      {/* if user not verified email show this modal  */}
+      {isEmailVarified && (
+        <div className={styles.mailVerifyModal}>
+          <h1>
+            Your Email is not verified. Check your mail.{' '}
+            <LinkButton
+              displayName={'Resend'}
+              icon={resend}
+              style={{ padding: '0.3rem 0.5rem', gap: '0.5rem' }}
+            />
+          </h1>
+        </div>
+      )}
     </section>
   );
 };
--- src/Components/UI/LinkButton.jsx
+++ src/Components/UI/LinkButton.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import styles from './LinkButton.module.css';
 
-const LinkButton = ({ displayName, path, icon }) => {
+const LinkButton = ({ displayName, path, icon, ...rest }) => {
   return (
     <Link to={path} className={styles.link}>
-      <span className={styles.button}>
+      <span className={styles.button} {...rest}>
         <img src={icon} />
         {displayName}
       </span>

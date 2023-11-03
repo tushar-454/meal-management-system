0d0ebc2ed058820f8fda57ad95c0657ea8c699f6
tushar-454 <imtushar454@gmail.com>
2023-11-03 12:06:33 +0600

    singup with email and password add input validation

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -6,7 +6,9 @@ import { Auth } from '../../Firebase/firebase-config';
 import password from '../../assets/icon/cyber-security.png';
 import google from '../../assets/icon/google.png';
 import email from '../../assets/icon/internet.png';
+import right from '../../assets/icon/right.png';
 import name from '../../assets/icon/user.png';
+import wrong from '../../assets/icon/wrong.png';
 import signupIllustration from '../../assets/sign_up.svg';
 import ButtonIco from '../Reusable/ButtonIco';
 import Container from '../Reusable/Container';
@@ -17,6 +19,7 @@ import Button from '../UI/Button';
 import Checkbox from '../UI/Checkbox';
 import Input from '../UI/Input';
 import InputFile from '../UI/InputFile';
+import styles from './Signup.module.css';
 
 const signupInit = {
   name: '',
@@ -34,24 +37,82 @@ const errorInit = {
   confirmPassword: '',
   terms: false,
 };
-
+const dynamicError = {
+  uppercase: false,
+  lowercase: false,
+  special: false,
+  length: false,
+};
 const Signup = () => {
   const { loginWithGoogle, signupWithEmailPassword } = useContext(AuthContext);
   const navigate = useNavigate();
   const [signup, setSignup] = useState({ ...signupInit });
   const [error, setError] = useState({ ...errorInit });
+  const [isShow, setIsShow] = useState(false);
 
+  //handle singup input change
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     const newValue = type === 'checkbox' ? checked : value;
     const newError = type === 'checkbox' ? checked : '';
     setSignup((prevObj) => ({ ...prevObj, [name]: newValue }));
     setError((prevError) => ({ ...prevError, [name]: newError }));
+    if (name === 'password') {
+      setIsShow(true);
+      // password strong check
+      if (value.length >= 6) {
+        dynamicError.length = true;
+      } else {
+        dynamicError.length = false;
+      }
+      if (/[A-Z]/.test(value)) {
+        dynamicError.uppercase = true;
+      } else {
+        dynamicError.uppercase = false;
+      }
+      if (/[a-z]/.test(value)) {
+        dynamicError.lowercase = true;
+      } else {
+        dynamicError.lowercase = false;
+      }
+      if (/[\W_]/.test(value)) {
+        dynamicError.special = true;
+      } else {
+        dynamicError.special = false;
+      }
+    }
   };
-
+  // handle singup with email and password
   const handleSignUpSubmit = (e) => {
     e.preventDefault();
     const { name, email, photoUrl, password, confirmPassword } = signup;
+    if (!name) {
+      setError((prevError) => ({
+        ...prevError,
+        name: 'Name is required !',
+      }));
+      return;
+    }
+    if (!email) {
+      setError((prevError) => ({
+        ...prevError,
+        email: 'Email is required !',
+      }));
+      return;
+    }
+    if (!password) {
+      setError((prevError) => ({
+        ...prevError,
+        password: 'Password is required !',
+      }));
+      return;
+    } else if (!/^(?=.*[A-Z])(?=.*[\W_]).{6,}$/.test(password)) {
+      setError((prevError) => ({
+        ...prevError,
+        password: '',
+      }));
+      return;
+    }
     if (password !== confirmPassword) {
       setError((prevError) => ({
         ...prevError,
@@ -134,6 +195,41 @@ const Signup = () => {
             error={error.confirmPassword}
             required
           />
+          {/* dynamicError show  */}
+          <div className={`${styles.hide} ${isShow && styles.block}`}>
+            <div
+              className={`${styles.errorLine} ${
+                dynamicError.lowercase ? styles.right : styles.wrong
+              }`}
+            >
+              <img src={dynamicError.lowercase ? right : wrong} width='16px' />
+              <p>Must one lowercase</p>
+            </div>
+            <div
+              className={`${styles.errorLine} ${
+                dynamicError.uppercase ? styles.right : styles.wrong
+              }`}
+            >
+              <img src={dynamicError.uppercase ? right : wrong} width='16px' />
+              <p>Must one uppercase</p>
+            </div>
+            <div
+              className={`${styles.errorLine} ${
+                dynamicError.special ? styles.right : styles.wrong
+              }`}
+            >
+              <img src={dynamicError.special ? right : wrong} width='16px' />
+              <p>Must one special character</p>
+            </div>
+            <div
+              className={`${styles.errorLine} ${
+                dynamicError.length ? styles.right : styles.wrong
+              }`}
+            >
+              <img src={dynamicError.length ? right : wrong} width='16px' />
+              <p>Password must be 6 characters</p>
+            </div>
+          </div>
           <Checkbox
             displayName={'Accept our terms and condition.'}
             id={'terms'}

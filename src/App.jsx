82ed5a9afa00d877fc40bd23fdd798056fcec50f
tushar-454 <imtushar454@gmail.com>
2023-11-17 10:59:25 +0600

    add meal from client using axios functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import AuthProvider from './AuthProvider/AuthProvider';
 import routes from './Routes/Routes';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <AuthProvider>
-      <RouterProvider router={routes} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={routes} />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/Components/AddMeal/AddMeal.jsx
+++ src/Components/AddMeal/AddMeal.jsx
@@ -1,17 +1,65 @@
+import { useState } from 'react';
+import Toast from '../../Utils/Toast/Toast';
 import breackfast from '../../assets/icon/breakfast.png';
 import dinner from '../../assets/icon/dinner.png';
 import launch from '../../assets/icon/lunch.png';
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 import Container from '../Reusable/Container';
 import MealFormLay from '../Reusable/MealFormLay';
 import PageTitle from '../Reusable/PageTitle';
 import Input from '../UI/Input';
 
+const mealInfoInit = {
+  date: '',
+  breackfast: '',
+  launch: '',
+  dinner: '',
+};
+
 const AddMeal = () => {
+  const [mealInfo, setMealInfo] = useState({ ...mealInfoInit });
+  const axios = useAxios();
+  const { user } = useAuth();
+
+  // handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setMealInfo((prevObj) => ({ ...prevObj, [name]: value }));
+  };
+
+  //handler add meal submit
+  const handleAddMealSubmit = (e) => {
+    e.preventDefault();
+    const { breackfast, launch, dinner } = mealInfo;
+    const date = new Date();
+    const mealInfoDate = {
+      uid: user.uid,
+      date: date.toLocaleDateString(),
+      breackfast,
+      launch,
+      dinner,
+    };
+    if (breackfast && launch && dinner) {
+      axios.post('/user/add-meal', mealInfoDate).then((res) => {
+        if (res.data.insertedId) {
+          Toast('Successfully add meal data.', 'success');
+        }
+        console.log(res.data);
+      });
+    } else {
+      Toast('Fillup all meal input box', 'info');
+    }
+  };
+
   return (
     <section>
       <Container>
         <PageTitle>Add your meal quantity</PageTitle>
-        <MealFormLay displayName={'Add Meal'}>
+        <MealFormLay
+          displayName={'Add Meal'}
+          handleSubmit={handleAddMealSubmit}
+        >
           <Input
             displayName={'Breackfast'}
             id={'breackfast'}
@@ -21,6 +69,9 @@ const AddMeal = () => {
             max={10}
             icon={breackfast}
             placeholder={'breackfast quentity'}
+            name={'breackfast'}
+            onChange={handleInputChange}
+            value={mealInfo.breackfast}
           />
           <Input
             displayName={'Launch'}
@@ -31,6 +82,9 @@ const AddMeal = () => {
             max={10}
             icon={launch}
             placeholder={'launch quentity'}
+            name={'launch'}
+            onChange={handleInputChange}
+            value={mealInfo.launch}
           />
           <Input
             displayName={'Dinner'}
@@ -41,6 +95,9 @@ const AddMeal = () => {
             max={10}
             icon={dinner}
             placeholder={'dinner quentity'}
+            name={'dinner'}
+            onChange={handleInputChange}
+            value={mealInfo.dinner}
           />
         </MealFormLay>
       </Container>
